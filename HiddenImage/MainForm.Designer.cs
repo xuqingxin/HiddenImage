2981d99108b764dddc5ab83413ce92e830d83385
徐庆新 <qingxin.xu@lebaitu.cn>
2021-11-01 11:46:15 +0800

    最终图片斜对角使用图片1和图片2的值

---
+++ HiddenImage/MainForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace HiddenImage
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbImage1 = new System.Windows.Forms.PictureBox();
+            this.pbImage2 = new System.Windows.Forms.PictureBox();
+            this.pbImage3 = new System.Windows.Forms.PictureBox();
+            this.btnImage1 = new System.Windows.Forms.Button();
+            this.btnImage2 = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImage1
+            // 
+            this.pbImage1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage1.Location = new System.Drawing.Point(12, 12);
+            this.pbImage1.Name = "pbImage1";
+            this.pbImage1.Size = new System.Drawing.Size(242, 289);
+            this.pbImage1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage1.TabIndex = 0;
+            this.pbImage1.TabStop = false;
+            // 
+            // pbImage2
+            // 
+            this.pbImage2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage2.Location = new System.Drawing.Point(271, 12);
+            this.pbImage2.Name = "pbImage2";
+            this.pbImage2.Size = new System.Drawing.Size(242, 289);
+            this.pbImage2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage2.TabIndex = 1;
+            this.pbImage2.TabStop = false;
+            // 
+            // pbImage3
+            // 
+            this.pbImage3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage3.Location = new System.Drawing.Point(532, 12);
+            this.pbImage3.Name = "pbImage3";
+            this.pbImage3.Size = new System.Drawing.Size(242, 289);
+            this.pbImage3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage3.TabIndex = 2;
+            this.pbImage3.TabStop = false;
+            // 
+            // btnImage1
+            // 
+            this.btnImage1.Location = new System.Drawing.Point(86, 324);
+            this.btnImage1.Name = "btnImage1";
+            this.btnImage1.Size = new System.Drawing.Size(75, 23);
+            this.btnImage1.TabIndex = 3;
+            this.btnImage1.Text = "选择图片";
+            this.btnImage1.UseVisualStyleBackColor = true;
+            this.btnImage1.Click += new System.EventHandler(this.btnImage1_Click);
+            // 
+            // btnImage2
+            // 
+            this.btnImage2.Location = new System.Drawing.Point(346, 324);
+            this.btnImage2.Name = "btnImage2";
+            this.btnImage2.Size = new System.Drawing.Size(75, 23);
+            this.btnImage2.TabIndex = 4;
+            this.btnImage2.Text = "选择图片";
+            this.btnImage2.UseVisualStyleBackColor = true;
+            this.btnImage2.Click += new System.EventHandler(this.btnImage2_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(612, 324);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "保存图片";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Image文件|*.bmp;*.jpg;*.jpeg;*.png";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "png";
+            this.saveFileDialog.FileName = "result.png";
+            this.saveFileDialog.Filter = "PNG文件|*.png";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 377);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnImage2);
+            this.Controls.Add(this.btnImage1);
+            this.Controls.Add(this.pbImage3);
+            this.Controls.Add(this.pbImage2);
+            this.Controls.Add(this.pbImage1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage1;
+        private System.Windows.Forms.PictureBox pbImage2;
+        private System.Windows.Forms.PictureBox pbImage3;
+        private System.Windows.Forms.Button btnImage1;
+        private System.Windows.Forms.Button btnImage2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
---
+++ HiddenImage/MainForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HiddenImage
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImage1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pbImage1.Image = Image.FromFile(openFileDialog.FileName);
+                GenerateImage();
+            }
+        }
+
+        private void btnImage2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pbImage2.Image = Image.FromFile(openFileDialog.FileName);
+                GenerateImage();
+            }
+        }
+
+        private void GenerateImage()
+        {
+            if (pbImage1.Image == null || pbImage2.Image == null)
+            {
+                return;
+            }
+
+            if (pbImage3.Image != null)
+            {
+                pbImage3.Image.Dispose();
+            }
+
+            Bitmap bmp1 = pbImage1.Image.Clone() as Bitmap;
+            Bitmap bmp2 = pbImage2.Image.Clone() as Bitmap;
+
+            if (bmp1.Width != bmp2.Width || bmp1.Height != bmp2.Height)
+            {
+                AdjustImage(bmp1, ref bmp2);
+            }
+            if (bmp1.PixelFormat == PixelFormat.Format32bppArgb)
+            {
+                ImageUtility.ARGB2Gray(ref bmp1);
+            }
+            else if (bmp1.PixelFormat == PixelFormat.Format24bppRgb)
+            {
+                ImageUtility.RGB2Gray(ref bmp1);
+            }
+            if (bmp2.PixelFormat == PixelFormat.Format32bppArgb)
+            {
+                ImageUtility.ARGB2Gray(ref bmp2);
+            }
+            else if (bmp2.PixelFormat == PixelFormat.Format24bppRgb)
+            {
+                ImageUtility.RGB2Gray(ref bmp2);
+            }
+
+            //ImageUtility.Inverse(bmp2);
+
+            int width = bmp1.Width;
+            int height = bmp1.Height;
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            Bitmap bmp3 = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            BitmapData bmp1Data = bmp1.LockBits(rect, ImageLockMode.ReadWrite, bmp1.PixelFormat);
+            BitmapData bmp2Data = bmp2.LockBits(rect, ImageLockMode.ReadWrite, bmp2.PixelFormat);
+            BitmapData bmp3Data = bmp3.LockBits(rect, ImageLockMode.ReadWrite, bmp3.PixelFormat);
+            int src_bytes1 = bmp1Data.Stride * height;
+            int src_bytes2 = bmp2Data.Stride * height;
+            int src_bytes3 = bmp3Data.Stride * height;
+            byte[] srcValues1 = new byte[src_bytes1];
+            byte[] srcValues2 = new byte[src_bytes2];
+            byte[] srcValues3 = new byte[src_bytes3];
+            System.Runtime.InteropServices.Marshal.Copy(bmp1Data.Scan0, srcValues1, 0, src_bytes1);
+            System.Runtime.InteropServices.Marshal.Copy(bmp2Data.Scan0, srcValues2, 0, src_bytes2);
+
+            int index = 1;
+            for (int i = 0; i < height; i++)
+            {
+                index = 1 - index;
+                for (int j = index; j < width; j += 2)
+                {
+                    byte val = (byte)(255 - srcValues1[i * bmp1Data.Stride + j]);
+                    srcValues3[i * bmp3Data.Stride + j * 4] = 0;
+                    srcValues3[i * bmp3Data.Stride + j * 4 + 1] = 0;
+                    srcValues3[i * bmp3Data.Stride + j * 4 + 2] = 0;
+                    srcValues3[i * bmp3Data.Stride + j * 4 + 3] = val;
+                }
+            }
+
+            index = 0;
+            for (int i = 0; i < height; i++)
+            {
+                index = 1 - index;
+                for (int j = index; j < width; j += 2)
+                {
+                    byte val = srcValues2[i * bmp2Data.Stride + j];
+                    srcValues3[i * bmp3Data.Stride + j * 4] = 255;
+                    srcValues3[i * bmp3Data.Stride + j * 4 + 1] = 255;
+                    srcValues3[i * bmp3Data.Stride + j * 4 + 2] = 255;
+                    srcValues3[i * bmp3Data.Stride + j * 4 + 3] = val;
+                }
+            }
+
+            System.Runtime.InteropServices.Marshal.Copy(srcValues3, 0, bmp3Data.Scan0, src_bytes3);
+            bmp1.UnlockBits(bmp1Data);
+            bmp2.UnlockBits(bmp2Data);
+            bmp3.UnlockBits(bmp3Data);
+
+            pbImage3.Image = bmp3;
+        }
+
+        private void AdjustImage(Bitmap bmp1, ref Bitmap bmp2)
+        {
+            Bitmap bmp = new Bitmap(bmp1.Width, bmp1.Height, bmp1.PixelFormat);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(bmp2, 0, 0, bmp.Width, bmp.Height);
+            bmp2.Dispose();
+            bmp2 = bmp;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (pbImage3.Image == null)
+            {
+                return;
+            }
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pbImage3.Image.Save(saveFileDialog.FileName, ImageFormat.Png);
+            }
+        }
+    }
+}
